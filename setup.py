86e922a4648fa83cf0b53bcf8e34926185586bcb
Denis Sazonov <den@saz.lt>
2019-06-15 00:37:24 +0100

    py35 and tox removed from travis

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ with open(req, 'rt', encoding="utf-8") as f:
 
 setup(name='jsonextra',
       version=__version__,
-      description='JSON Extra | JSON that gives you extra',
+      description='JSON Extra | JSON that gives you extra datetime and uuid data types',
       author='Denis Sazonov',
       author_email='den@saz.lt',
       packages=find_packages(),
       license='Restricted',
       install_requires=install_requires,
       include_package_data=True,
-      python_requires=">=3.5",
+      python_requires=">=3.6",
       zip_safe=True)
