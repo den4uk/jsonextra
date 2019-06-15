c7d570df3399edf67c8f8587027c344c4507e9fd
Denis Sazonov <den@saz.lt>
2019-06-15 09:07:51 +0100

    setup coverall and badges

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 JSON Extra
 =====
 [![Build Status](https://travis-ci.org/den4uk/jsonextra.svg?branch=master)](https://travis-ci.org/den4uk/jsonextra)
-
+[![Coverage Status](https://coveralls.io/repos/github/den4uk/jsonextra/badge.svg?branch=master)](https://coveralls.io/github/den4uk/jsonextra?branch=master)
 
 ## Installation
 
--- setup.py
+++ setup.py
@@ -3,18 +3,33 @@ from setuptools import setup, find_packages
 from jsonextra import __version__
 
 req = os.path.join(os.path.dirname(__file__), 'requirements.txt')
-with open(req, 'rt', encoding="utf-8") as f:
+with open(req, 'rt', encoding='utf-8') as f:
     install_requires = [dep for dep in f.read().splitlines() if not dep.startswith('#')]
 
 
-setup(name='jsonextra',
-      version=__version__,
-      description='JSON Extra | JSON that gives you extra datetime and uuid data types',
-      author='Denis Sazonov',
-      author_email='den@saz.lt',
-      packages=find_packages(),
-      license='Restricted',
-      install_requires=install_requires,
-      include_package_data=True,
-      python_requires=">=3.6",
-      zip_safe=True)
+reme = os.path.join(os.path.dirname(__file__), 'README.md')
+with open(reme, 'rt', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='jsonextra',
+    version=__version__,
+    description='JSON Extra | JSON that gives you extra datetime and uuid data types',
+    author='Denis Sazonov',
+    author_email='den@saz.lt',
+    packages=find_packages(),
+    license='Restricted',
+    install_requires=install_requires,
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/den4uk/jsonextra",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    zip_safe=True,
+)
